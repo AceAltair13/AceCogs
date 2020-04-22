c5277583f971cf2a03baf2b32be71f584e2c7e0e
AceAltair13 <tirththoria13@hotmail.com>
2020-04-22 23:27:58 +0530

    Initial changes

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .coinrush import CoinRush
+
+def setup(bot):
+    bot.add_cog(CoinRush())
---
+++ coinrush.py
@@ -0,0 +1,12 @@
+from redbot.core import commands
+
+class CoinRush(commands.Cog):
+    """A Small Movement Based Coin Collecting Minigame"""
+    @commands.command()
+    async def start(self,ctx):
+        """Starts a game of coin rush"""
+        pass
+
+    @commands.command()
+    async def stop(self,ctx):
+        pass
