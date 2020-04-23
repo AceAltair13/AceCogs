a88e8188160dc863e487be5a08abc0f3be6cf013
AceAltair13 <tirththoria13@hotmail.com>
2020-04-23 21:14:24 +0530

    Updated

--- coingame.py
+++ coingame.py
@@ -12,22 +12,11 @@ import random
 from enum import Enum
 
 
-class Cell:
-    def __init__(self):
-        self.has = Pickup["empty"]
-        self.visible = False
-
-    def reveal(self):
-        self.visible = True
-
-    def collect(self):
-        self.has = Pickup["empty"]
-
-    def set_pickup(self, has):
-        self.has = has
-
-    def is_empty(self):
-        return self.has == Pickup["empty"]
+class Direction(Enum):
+    right = 0
+    up = 1
+    left = 2
+    down = 3
 
 
 class Pickup(Enum):
@@ -37,24 +26,93 @@ class Pickup(Enum):
     reveal = 3
 
 
-class CoinGame:
+KEYS = {
+    'w': Direction.up,
+    'a': Direction.left,
+    's': Direction.down,
+    'd': Direction.right
+}
+
+
+class Cell:
     def __init__(self):
-        self.map = {}
+        self.has = Pickup["empty"]
+        self.visible = False
+
+    def reveal(self):
+        self.visible = True
+
+    def collect(self):
+        ret = self.has
+        self.has = Pickup["empty"]
+        return ret
+
+    def set_pickup(self, has):
+        self.has = has
+
+    def is_empty(self):
+        return self.has == Pickup["empty"]
+
+
+class CoinGame:
+
+    def __init__(self):
+
+        self.stats = {
+            'coins': 0,
+            'power': 0,
+            'reveal': 0,
+            'max_coins': 0,
+            'max_power': 0,
+            'max_reveal': 0,
+            'moves': 25
+        }
+        self.player = (5, 0)
+        self.coinmap = {}
+
         for i in range(9):
             for j in range(9):
-                self.map[(i, j)] = Cell()
+                self.coinmap[(i, j)] = Cell()
+        empty_cells = list(self.coinmap.values())
+        empty_cells.remove(self.coinmap[self.player])
 
         def put_stuff(self, item, wt1, wt2):
-            self.qty = random.randint(self.wt1, self.wt2)
-            while self.qty:
-                self.x, self.y = random.randint(0, 8), random.randint(0, 8)
-                if self.map[(self.x, self.y)].is_empty():
-                    if self.x != 5 and self.y != 0:
-                        self.map[(self.x, self.y)].set_pickup(item)
-                        self.qty -= 1
-        put_stuff(self, Pickup['coin'], 35, 45)
-        put_stuff(self, Pickup['power'], 10, 15)
-        put_stuff(self, Pickup['reveal'], 0, 1)
+            qty = random.randint(wt1, wt2)
+            for i in range(qty):
+                cell = random.choice(empty_cells)
+                empty_cells.remove(cell)
+                cell.set_pickup(item)
+            return qty
+
+        self.stats['max_coins'] = put_stuff(self, Pickup['coin'], 35, 45)
+        self.stats['max_power'] = put_stuff(self, Pickup['power'], 10, 15)
+        self.stats['max_reveal'] = put_stuff(self, Pickup['reveal'], 0, 1)
+
+    def move_player(self, dir):
+        # | 0: right | 1: up | 2: left | 3: down |
+        x, y = self.player
+        if dir == 0:
+            if not x == 8:
+                x += 1
+        elif dir == 1:
+            if not y == 8:
+                y += 1
+        elif dir == 2:
+            if not x == 0:
+                x -= 1
+        elif dir == 3:
+            if not y == 0:
+                y -= 1
+        self.player = (x, y)
+        item_ = self.coinmap[(x, y)].collect()
+        if item_ == Pickup.coin:
+            self.stats["coins"] += 1
+        elif item_ == Pickup.power:
+            self.stats["power"] += 1
+        elif item_ == Pickup.reveal:
+            self.stats["reveal"] += 1
+        self.coinmap[(x, y)].reveal()
+        self.stats['moves'] -= 1
 
 
 def get_key(check, *, print_keys=False):
@@ -67,10 +125,12 @@ def get_key(check, *, print_keys=False):
             return result
 
 
-KEYS = ('w', 'a', 's', 'd')
+def check(key):
+    return KEYS.get(key)
 
 
-def key_check(key):
-    if key in KEYS:
-        return key
-    return None
+game = CoinGame()
+
+while True:
+    way = get_key(check)
+    game.move_player(way)
